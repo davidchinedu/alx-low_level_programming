3e29e4e790ff93f75916b29a98a774db0c73b894
davidchinedu <chinedudavid241@gmail.com>
2023-03-16 13:48:09 -0700

    h

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/**
+ * main - a program that prints a line with pts function
+ * return : 0 (success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
