a6718adce84eae3147d05b73710dd7dd2c339547
davidchinedu <chinedudavid241@gmail.com>
2023-05-04 10:43:03 -0700

    a

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,7 +9,7 @@
 unsigned int binary_to_uint(const char *b)
 {
 int i;
-unsigned int value = 0;
+unsigned int dec_val = 0;
 
 if (!b)
 return (0);
@@ -18,8 +18,8 @@ for (i = 0; b[i]; i++)
 {
 if (b[i] < '0' || b[i] > '1')
 return (0);
-value = 2 * value + (b[i] - '0');
+dec_val = 2 * dec_val + (b[i] - '0');
 }
 
-return (value);
+return (dec_val);
 }
